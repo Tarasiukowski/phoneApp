b750d79844d490857bb496c125a4fe2996d164b3
Tarasiukowski <tarasiuk.michal03@gmail.com>
2021-03-26 12:11:34 +0100

    fix onboarding route push

--- frontend/components/organisms/onBoardingContent/number/number.tsx
+++ frontend/components/organisms/onBoardingContent/number/number.tsx
@@ -2,12 +2,15 @@ import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import SelectNumberButton from '../../../atoms/selectNumber/button/button';
 import SelectNumberList from '../../../atoms/selectNumber/list/list';
+import RedirectTemplate from '../../../../templates/redirectTemplate/redirectTemplate';
 import { Button } from '../../../atoms/button/button';
 import { selectUser } from '../../../../reducers/userReducer';
 import styles from './number.module.scss';
+import axios from 'axios';
 
 const OnboardingNumberContent = () => {
   const [openList, setOpenList] = useState<boolean>(false);
+  const [redirect, setRedirect] = useState<boolean>(false);
   const [number, setNumber] = useState<string | null>(null);
 
   const user = useSelector(selectUser);
@@ -20,16 +23,27 @@ const OnboardingNumberContent = () => {
     setOpenList(!openList);
   };
 
+  const next = () => {
+    axios.post('http://localhost:7000/user/update', {
+      email: user.email,
+      number,
+    });
+
+    setRedirect(true);
+  };
+
   return (
-    <div className={styles.wrapper}>
-      <h4>Your phone number</h4>
-      <h6>You can add more phone numbers later.</h6>
-      <SelectNumberButton onClick={toggleOpenList} number={number} />
-      <Button disabled={!number} margin="32px 0 0 0" width="100%">
-        Continue
-      </Button>
-      {openList && <SelectNumberList setNumber={setNumber} setOpenList={setOpenList} />}
-    </div>
+    <RedirectTemplate redirectTo="/onboarding/account" isRedirect={redirect}>
+      <div className={styles.wrapper}>
+        <h4>Your phone number</h4>
+        <h6>You can add more phone numbers later.</h6>
+        <SelectNumberButton onClick={toggleOpenList} number={number} />
+        <Button onClick={next} disabled={!number} margin="32px 0 0 0" width="100%">
+          Continue
+        </Button>
+        {openList && <SelectNumberList setNumber={setNumber} setOpenList={setOpenList} />}
+      </div>
+    </RedirectTemplate>
   );
 };
 
