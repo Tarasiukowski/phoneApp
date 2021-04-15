84d6b8e5af0d4eda924fd1d4d121f70e1003e487
Tarasiukowski <tarasiuk.michal03@gmail.com>
2021-04-15 22:56:10 +0200

    fix alert component

--- frontend/components/atoms/alert/alert.tsx
+++ frontend/components/atoms/alert/alert.tsx
@@ -5,8 +5,18 @@ import styles from './alert.module.scss';
 
 const Alert = ({ errorMsg }: propsAlert) => {
   const [isOpen, setIsOpen] = useState<boolean>(Boolean);
+  const [msg, setMsg] = useState<string | null>(null);
 
   useEffect(() => {
+    if (errorMsg) {
+      if (errorMsg.length - 49 > 20) {
+        const sliceMsg = errorMsg.substring(0, 67) + '...';
+
+        setMsg(sliceMsg);
+      } else {
+        setMsg(errorMsg);
+      }
+    }
     setIsOpen(true);
   }, [errorMsg]);
 
@@ -18,7 +28,7 @@ const Alert = ({ errorMsg }: propsAlert) => {
     <>
       {errorMsg && isOpen ? (
         <div className={styles.wrapper}>
-          <p>{errorMsg}</p>
+          <p>{msg}</p>
           <Button onClick={close} transparent alert>
             Close
           </Button>
