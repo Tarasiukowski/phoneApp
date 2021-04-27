0a917efea5e2c44cea7c9ab501665c5c98a529f9
Tarasiukowski <tarasiuk.michal03@gmail.com>
2021-04-27 19:12:27 +0200

    fix file structure

---
+++ client/pages/settings/[...slug].tsx
@@ -0,0 +1,5 @@
+import MainTemplate from '../../templates/mainTemplate/mainTemplate';
+
+const SettingsPage = () => <MainTemplate></MainTemplate>;
+
+export default SettingsPage;
--- client/templates/mainTemplate/mainTemplate.tsx
+++ client/templates/mainTemplate/mainTemplate.tsx
@@ -1,10 +1,17 @@
+import styled from "styled-components"
 import Navigation from "../../components/molecules/navigation/navigation";
 
 const MainTemplate: React.FC = ({ children }) => (
-  <>
+  <Template>
     <Navigation />
     {children}
-  </>
+  </Template>
 )
+
+const Template = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+`
 
 export default MainTemplate
