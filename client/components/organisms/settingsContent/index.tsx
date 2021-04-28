47e77654fb394f1d5fa6def770c7cd5f377f5d5a
Tarasiukowski <tarasiuk.michal03@gmail.com>
2021-04-28 11:09:11 +0200

    fix pages file structure

---
+++ client/components/organisms/settingsContent/index.tsx
@@ -0,0 +1,11 @@
+import SettingsNavigation from '../../molecules/settingsNavigation/settingsNavigation';
+// import SettingsProfileContent from './profile/profile';
+
+const SettingsContent = () => (
+  <>
+    <SettingsNavigation />
+    {/* <SettingsProfileContent /> */}
+  </>
+);
+
+export default SettingsContent;
---
+++ client/pages/[...slug].tsx
@@ -0,0 +1,10 @@
+import SettingsContent from '../components/organisms/settingsContent';
+import MainTemplate from '../templates/mainTemplate/mainTemplate';
+
+const MainPage = () => (
+  <MainTemplate>
+    <SettingsContent />
+  </MainTemplate>
+);
+
+export default MainPage;
