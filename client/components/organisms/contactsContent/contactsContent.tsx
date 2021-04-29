05b88e2b5c173e5f1d169283ff29d64146b66a56
Tarasiukowski <tarasiuk.michal03@gmail.com>
2021-04-29 18:46:29 +0200

    create contactsContent

---
+++ client/components/organisms/contactsContent/contactsContent.tsx
@@ -0,0 +1,31 @@
+import UserDetailed from '../chatContent/userDetailed/userDetailed';
+import styles from './contactsContent.module.scss';
+import UserCard from '../../atoms/userCard/userCard';
+import { SearchSvg } from "../../../public/svgs"
+
+const ContactsContent = () => (
+  <>
+    <div className={styles.template}>
+      <div className={styles.header}>
+        <p>Friends</p>
+        <span>1</span>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.inputTemplate}>
+          <div>
+            <SearchSvg />
+          </div>
+          <div>
+            <input placeholder="Search" />
+          </div>
+        </div>
+        <div className={styles.listElement}>
+           <UserCard />
+        </div>
+      </div>
+    </div>
+    <UserDetailed />
+  </>
+);
+
+export default ContactsContent;
--- client/pages/[...slug].tsx
+++ client/pages/[...slug].tsx
@@ -1,11 +1,12 @@
 // import SettingsContent from '../components/organisms/settingsContent';
-import ChatContent from '../components/organisms/chatContent/chatContent';
+// import ChatContent from '../components/organisms/chatContent/chatContent';
+import ContactsContent from '../components/organisms/contactsContent/contactsContent';
 import MainTemplate from '../templates/mainTemplate/mainTemplate';
 
 const MainPage = () => (
   <MainTemplate>
     {/* <SettingsContent /> */}
-    <ChatContent />
+    <ContactsContent />
   </MainTemplate>
 );
 
