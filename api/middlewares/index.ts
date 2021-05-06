8a36ed37ae7b0c7a97bd11d55252868d5fdf8702
Tarasiukowski <tarasiuk.michal03@gmail.com>
2021-05-06 16:12:39 +0200

    create userMiddleware

--- api/middlewares/index.ts
+++ api/middlewares/index.ts
@@ -1,6 +1,7 @@
 import { Application } from 'express';
 import generateMiddleware from './generateMiddleware';
+import userMiddleware from './userMiddleware';
 
 export const useMiddlewares = (app: Application) => {
-  app.use('/generate', generateMiddleware.index);
+  app.use('/generate', generateMiddleware.index).use('/user', userMiddleware.index);
 };
---
+++ api/middlewares/userMiddleware.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from 'express';
+import userService from '../services/userService';
+
+class userMiddleware {
+  async index(req: Request, res: Response, next: NextFunction) {
+    const { email } = req.body
+    const token = req.cookies.SESSID;
+
+    const { user } = await userService.loginByToken(token);
+
+    if (user.email === email) {
+      next();
+      return;
+    }
+
+    res.send({ error: true, msg: 'function not allowed' });
+  }
+}
+
+const UserMiddleware = new userMiddleware();
+
+export default UserMiddleware;
