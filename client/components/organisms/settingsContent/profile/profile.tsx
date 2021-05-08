7de448e7648116a6d4b54ed76e1ac33ff0da2db1
Tarasiukowski <tarasiuk.michal03@gmail.com>
2021-05-08 21:25:38 +0200

    add functionality to settingsContentProfile

--- client/components/organisms/settingsContent/profile/profile.tsx
+++ client/components/organisms/settingsContent/profile/profile.tsx
@@ -1,32 +1,93 @@
+import { ChangeEvent, useReducer, useState } from 'react';
+import { useSelector } from 'react-redux';
+
 import SettingsTemplate from '../../../../templates/settingsTemplate/settingsTemplate';
 import ImageUser from '../../../atoms/imageUser/imageUser';
 import { Input } from '../../../atoms/input/input';
 import { Button } from '../../../atoms/button/button';
+import Alert from '../../../atoms/alert/alert';
 
 import styles from './profile.module.scss';
+import { InputsValues } from './types';
+import { selectUser } from '../../../../reducers/userReducer';
+import { fetcher } from '../../../../utils';
+import { Error } from '../../../../interfaces';
 
-const SettingsProfileContent = () => (
-  <SettingsTemplate>
-    <h2 className="title">Account</h2>
-    <p className="description">Manage your OpenPhone profile.</p>
-    <div className={styles.templateImage}>
-      <ImageUser size="90%" fontSize="3rem" />
-    </div>
-    <div className={styles.templateInputs}>
-      <Input name="firstname" value="Michał" placeholder="First name" autoComplete="off" />
-      <Input name="lastname" value="Tarasiuk" placeholder="Last name" autoComplete="off" />
-    </div>
-    <p className={styles.label}>Email</p>
-    <div className={styles.emailSet}>
-      <p>michal.tarasiuk03@gmail.com</p>
-      <Button width="auto" transparent>
-        Change
+const SettingsProfileContent = () => {
+  const [error, setError] = useState<Error | null>(null);
+
+  const { firstname, lastname, email } = useSelector(selectUser);
+
+  const [inputsValues, setInputsValues] = useReducer(
+    (prevState: InputsValues, state: InputsValues) => ({ ...prevState, ...state }),
+    { firstname, lastname },
+  );
+
+  const { firstname: firstnameValue, lastname: lastnameValue } = inputsValues;
+
+  const handleOnChange = (e: ChangeEvent) => {
+    const target = e.target as HTMLInputElement;
+
+    setInputsValues({
+      [target.name]: target.value,
+    });
+  };
+
+  const save = async () => {
+    if (firstnameValue !== firstname || lastnameValue !== lastname) {
+      const data = await fetcher('PUT', 'user/update', {
+        email,
+        firstname: firstnameValue,
+        lastname: lastnameValue,
+      });
+
+      if (data.error) {
+        setError({ msg: data.errorMsg, id: Math.random() });
+      }
+
+      window.location.reload();
+      return;
+    }
+
+    setError({ msg: 'error - name and surname are the same', id: Math.random() });
+  };
+
+  return (
+    <SettingsTemplate>
+      <h2 className="title">Account</h2>
+      <p className="description">Manage your OpenPhone profile.</p>
+      <div className={styles.templateImage}>
+        <ImageUser size="90%" fontSize="3rem" />
+      </div>
+      <div className={styles.templateInputs}>
+        <Input
+          name="firstname"
+          value={firstnameValue}
+          placeholder="Firstname"
+          autoComplete="off"
+          onChange={handleOnChange}
+        />
+        <Input
+          name="lastname"
+          value={lastnameValue}
+          onChange={handleOnChange}
+          placeholder="Lastname"
+          autoComplete="off"
+        />
+      </div>
+      <p className={styles.label}>Email</p>
+      <div className={styles.emailSet}>
+        <p>michal.tarasiuk03@gmail.com</p>
+        <Button width="auto" transparent>
+          Change
+        </Button>
+      </div>
+      <Button onClick={save} disabled={false} width="auto">
+        save
       </Button>
-    </div>
-    <Button disabled={false} width="auto">
-      save
-    </Button>
-  </SettingsTemplate>
-);
+      <Alert error={error} />
+    </SettingsTemplate>
+  );
+};
 
 export default SettingsProfileContent;
---
+++ client/components/organisms/settingsContent/profile/types.ts
@@ -0,0 +1,4 @@
+export type InputsValues = {
+  firstname?: string;
+  lastname?: string;
+};
