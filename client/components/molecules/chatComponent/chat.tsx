3bbf346714ad202dcd51a3f64577dcba7b3c6594
Tarasiukowski <tarasiuk.michal03@gmail.com>
2021-05-21 18:20:25 +0200

    modified chatComponent

--- client/components/molecules/chatComponent/chat.tsx
+++ client/components/molecules/chatComponent/chat.tsx
@@ -1,16 +1,11 @@
-import ImageUser from '../../atoms/imageUser/imageUser';
-import ChatOptions from './chatOptions/chatOptions';
-import { Textarea } from './textarea/textarea'
+import Header from './header/header';
+import { Textarea } from './textarea/textarea';
 
 import styles from './chat.module.scss';
 
 const Chat = () => (
   <div className={styles.template}>
-    <div className={styles.header}>
-      <ImageUser />
-      <p className={styles.name}>Michał Tarasiuk</p>
-      <ChatOptions />
-    </div>
+    <Header />
     <div className={styles.content}>
       
     </div>
