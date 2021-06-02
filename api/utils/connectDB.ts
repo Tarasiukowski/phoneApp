9071f3a7ab3a230ea23f26a2b2f6ad3b42f382d5
Tarasiukowski <tarasiuk.michal03@gmail.com>
2021-06-02 21:55:00 +0200

    fix connectDB util

--- api/utils/connectDB.ts
+++ api/utils/connectDB.ts
@@ -1,9 +1,13 @@
 import * as mongoose from 'mongoose';
 
 export const connectDB = () => {
-  mongoose.connect('mongodb://127.0.0.1:27017/main', {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useFindAndModify: true,
-  });
+  try {
+    mongoose.connect('mongodb://127.0.0.1:27017/main', {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useFindAndModify: true,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
