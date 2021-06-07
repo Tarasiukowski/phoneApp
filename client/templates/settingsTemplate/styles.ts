ec39baac398cfbddf1a84b127247d63f0f3792c6
Tarasiukowski <tarasiuk.michal03@gmail.com>
2021-06-07 23:45:34 +0200

    add styles file to settingsTemplate

---
+++ client/templates/settingsTemplate/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Template = styled.div`
+  height: 100%;
+  width: 70.4vw;
+  background-color: #1a1925;
+  border-left: 1px solid rgba(255, 255, 255, 0.06);
+  padding: 2.5rem 0 1.5rem 3.1rem;
+
+  .title {
+    color: white;
+    font-size: 3.2rem;
+  }
+
+  .description {
+    color: rgb(151, 151, 176);
+    font-size: 13px;
+    font-weight: 600;
+    margin-top: 10px;
+  }
+`;
