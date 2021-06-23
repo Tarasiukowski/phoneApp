31f8cc56c86964b7cc1c8b5e7ad4595539003b55
Tarasiukowski <tarasiuk.michal03@gmail.com>
2021-06-23 21:30:38 +0200

    modified: ButtonGoogle

--- client/components/atoms/buttonGoogle/index.tsx
+++ client/components/atoms/buttonGoogle/index.tsx
@@ -2,11 +2,15 @@ import LogoGoole from '../../../public/svgs/googleLogo.svg';
 import { props } from './types';
 import styles from './buttonGoogle.module.scss';
 
-const ButtonGoogle = ({ auth, onClick }: props) => (
-  <button onClick={onClick} className={styles.button}>
-    <LogoGoole />
-    {auth === 'login' ? 'Log in' : 'Sing up'} with <span>Google</span>
-  </button>
-);
+const ButtonGoogle = ({ auth, onClick }: props) => {
+  const isRegister = auth === 'singup';
 
-export { ButtonGoogle }
+  return (
+    <button onClick={onClick} className={styles.button}>
+      <LogoGoole />
+      {isRegister ? 'Sing up' : 'Log in'} with <span>Google</span>
+    </button>
+  );
+};
+
+export { ButtonGoogle };
