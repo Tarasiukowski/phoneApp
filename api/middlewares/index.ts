fcd40ef9187937993bfcedebd6ac1af7b094f2ee
Tarasiukowski <tarasiuk.michal03@gmail.com>
2021-07-19 21:53:30 +0200

    fix: useMiddlewares

--- api/middlewares/index.ts
+++ api/middlewares/index.ts
@@ -1,14 +1,7 @@
 import { Application } from 'express';
 
-import generateMiddleware from './generateMiddleware';
-import mainMiddleware from './mainMiddleware';
-import userMiddleware from './userMiddleware';
-import conversationMiddleware from './userMiddleware';
+import { mainMiddleware } from './mainMiddleware';
 
 export const useMiddlewares = (app: Application) => {
-  app
-    .use('/', mainMiddleware.index)
-    .use('/generate', generateMiddleware.index)
-    .use('/user', userMiddleware.index)
-    .use('/conversation', conversationMiddleware.index);
+  app.use('/', mainMiddleware);
 };
--- api/middlewares/mainMiddleware.ts
+++ api/middlewares/mainMiddleware.ts
@@ -1,27 +1,27 @@
 import { Request, Response, NextFunction } from 'express';
 
 import AuthService from '../services/authService';
+import { ERROR } from '../data';
 
-class mainMiddleware {
-  async index(req: Request, _: Response, next: NextFunction) {
-    const token = req.cookies.SESSID;
+const mainMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+  const token = req.cookies.SESSID;
 
-    if (token) {
-      const { user } = await AuthService.index(token);
+  const { user } = await AuthService.index(token);
 
-      if (user) {
-        if (user.value) {
-          const { email } = user.value;
-
-          req.body = { ...req.body, email };
-        }
-      }
+  if (user) {
+    if (user.value) {
+      const { email } = user.value;
+      
+      req.body = { ...req.body, email };
     }
-
-    next();
+  } else {
+    if (!req.path.startsWith('/auth')) {
+      res.status(405).send({ errorMsg: ERROR.FUNCTIONALITY_NOT_ALLOWED });
+      return;
+    }
   }
-}
 
-const MainMiddleware = new mainMiddleware();
+  next();
+};
 
-export default MainMiddleware;
+export { mainMiddleware };
